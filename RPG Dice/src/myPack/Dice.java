aa7d011b80a2e54cfe8873b113781826bda4d6f3
Stephen DeRosa <stephen_derosa@outlook.com>
2023-08-17 19:30:57 -0400

    Update Dice.java
    Created a while loop to keep the dialog box open.
    
    Removed the different-sided dice variables. Simplified to one dice variable to accept the user input.
    
    Used an if statement to implement user input to print out the dice roll.

--- RPG Dice/src/myPack/Dice.java
+++ RPG Dice/src/myPack/Dice.java
@@ -6,23 +6,15 @@ public class Dice {
 	
 	public static void main(String[] args) {
 		
-		String side = JOptionPane.showInputDialog("What sided dice do you need, 4, 6, 8, 10, 12, or 20?");
-		
-		// creating random number dice rolls
-		int d4 = (int)(Math.random() * 4) + 1;
-		int d6 = (int)(Math.random() * 6) + 1;
-		int d8 = (int)(Math.random() * 8) + 1;
-		int d10 = (int)(Math.random() * 10) + 1;
-		int d12 = (int)(Math.random() * 12) +1;
-		int d20 = (int)(Math.random() *20) +1;
+		while (true) {
+			String side = JOptionPane.showInputDialog("What sided dice do you need, 4, 6, 8, 10, 12, or 20?");
+			int s = Integer.parseInt(side);
 			
-		
-		// testing to make sure they produce correctly
-		System.out.println("4 sided "+d4);
-		System.out.println("6 sided "+d6);
-		System.out.println("8 " +d8);
-		System.out.println("10 "+d10);
-		System.out.println("12 "+d12);
-		System.out.println("20 "+d20);
-	}
-}
+			if (s == 4 || s == 6 || s == 8 || s == 10 || s == 12 || s == 20); {
+				int d = (int)(Math.random() * s) + 1;
+				JOptionPane.showMessageDialog(null, "Your dice roll is " +d);
+			} // if closing brace
+		} // while closing brace
+	} // main closing brace
+} // class closing brace
+
